a0c93102cd51c312a71524e2a2683e2225fb4d5e
suminiee <suminn0816@naver.com>
2024-10-17 14:40:57 +0900

    Update index.js

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,35 +1,35 @@
 import Vue from 'vue';
-import VueRouter from 'vue-router';
-import MainPage from '../components/MainPage.vue';   
-import LoginPage from '../components/LoginPage.vue';
+import Router from 'vue-router';
+import HelloWorld from '@/components/HelloWorld.vue';
+import LoginPage from '@/components/LoginPage.vue';
+import MainPage from '@/components/MainPage.vue';
 import SignupPage from '@/components/SignupPage.vue';
 
-// VueRouter를 사용하도록 Vue에 등록
-Vue.use(VueRouter);
+Vue.use(Router);
 
-const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: MainPage,  // MainPage를 기본 경로로 사용
-  },
-  {
-    path: '/login',
-    name: 'Login',
-    component: LoginPage,
-  },
-  {
-    path: '/signup',
-    name: 'Signup',
-    component: SignupPage,
-  }
-];
-
-const router = new VueRouter({
-  mode: 'history',  // history 모드를 사용하여 브라우저 히스토리를 관리
-  base: process.env.BASE_URL,
-  routes,
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'HelloWorld',
+      component: HelloWorld,  // 기본 경로는 HelloWorld
+    },
+    {
+      path: '/login',
+      name: 'LoginPage',
+      component: LoginPage,   // /login 경로는 LoginPage
+    },
+    {
+      path: '/main',
+      name: 'MainPage',
+      component: MainPage,    // /main 경로는 MainPage
+    },
+    {
+      path: '/signup',
+      name: 'SignupPage',
+      component: SignupPage,  // /signup 경로는 SignupPage
+    },
+  ],
 });
 
-export default router;
 
