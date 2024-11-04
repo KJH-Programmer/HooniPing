0c279fb6c50f9360d8e6062dc83d7c6887bbdafa
eojiinn <djwls2726@gmail.com>
2024-11-04 17:31:20 +0900

    광고 문구 생성, 프론트 연결

--- frontend/src/api/GptService.js
+++ frontend/src/api/GptService.js
@@ -18,30 +18,30 @@ export const ExtractKeyword = async (product) => {
 
 //==================== 문구 ======================
 // 광고 문구 출력
-export const GenerateAdText = async (product, brand, ad_format, tone, brand_model, features) => {
+export const GenerateAdText = async (product, brand, tone, brand_model, features, keywords) => {
     return await axios.post(`${API_URL}/adtext`, {
         product: product,
         brand: brand,
-        ad_format: ad_format,
+        features: features,
         tone: tone,
         brand_model: brand_model,
-        features: features
+        keywords: keywords
     })
 }
 
-//==================== 이미지 =======================
-// image 생성 (> s3 버킷에 저장 > 이미지 url 반환)
-export const GenerateAdImage = async (prompt, userId, campaignId) => {
-    return await axios.post(`${API_URL}/image`, {
-        prompt: prompt,
-        userId: userId,
-        campaignId: campaignId
-    })
-}
+// //==================== 이미지 =======================
+// // image 생성 (> s3 버킷에 저장 > 이미지 url 반환)
+// export const GenerateAdImage = async (prompt, userId, campaignId) => {
+//     return await axios.post(`${API_URL}/image`, {
+//         prompt: prompt,
+//         userId: userId,
+//         campaignId: campaignId
+//     })
+// }
 
-// image 버킷에서 삭제
-export const DeleteAdImage = async (prompt, userId, campaignId) => {
-    return await axios.post(`${API_URL}/image-dlt`, {
-        fileName: fileName
-    })
-}
+// // image 버킷에서 삭제
+// export const DeleteAdImage = async (prompt, userId, campaignId) => {
+//     return await axios.post(`${API_URL}/image-dlt`, {
+//         fileName: fileName
+//     })
+// }
