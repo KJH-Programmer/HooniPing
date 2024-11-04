54a2b39b9a4fac84998cb0fed51542bcd5b83a79
eojiinn <djwls2726@gmail.com>
2024-11-04 16:16:03 +0900

    캠페인 Entity(데이터베이스 column) 수정

--- backend/src/main/java/wwee/jihun/Entity/CampaignEntity.java
+++ backend/src/main/java/wwee/jihun/Entity/CampaignEntity.java
@@ -17,56 +17,39 @@ import javax.annotation.processing.Generated;
 public class CampaignEntity {
 
     @Id
-    @Column(name = "campaign_id", updatable = true)
+    @Column(name = "campaign_id", updatable = true)  // 캠페인 id
     private Long campaignId;
 
-    @Column(name = "user_id")
+    @Column(name = "user_id")  // 사용자 id
     private String userId;
 
-    @Column(name = "product")
+    //=============입력 필수==============
+    @Column(name = "product")  // 제품명
     private String product;
 
-    @Column(name = "keyword1")
-    private String keyword1;
-
-    @Column(name = "keyword2")
-    private String keyword2;
-
-    @Column(name = "keyword3")
-    private String keyword3;
-
-    @Column(name = "keywords")
+    @Column(name = "keywords")  // 선택한 키워드 (","로 구분)
     private String keywords;
 
-    @Column(name = "brand")
-    private String brand;
+    @Column(name = "features")  // 제품 특장점(필수)
+    private String features;
 
-    @Column(name = "tone")
+    //=============입력 선택(옵션)==============
+    @Column(name = "tone")  // 어조(옵션, default='습니다체')
     private String tone;
 
-    @Column(name = "prompt_for_ad_text")
-    private String prompt_for_ad_text;
+    @Column(name = "brand")  // 브랜드명(옵션)
+    private String brand;
 
-    @Column(name = "prompt_for_ad_image")
-    private String prompt_for_ad_image;
-
-    @Column(name = "ad_text")
-    private String ad_text;
-
-    @Column(name = "ad_image")
-    private String ad_image;
-
-    @Column(name = "ad_format")
-    private String ad_format;
-
-    @Column(name = "image_url")
-    private String image_url;
-
-    @Column(name = "brand_model")
+    @Column(name = "brand_model")  // 광고 모델 이름(옵션)
     private String brand_model;
 
-    @Column(name = "features")
-    private String features;
+    // 생성된 광고 문구 & 이미지
+    @Column(name = "ad_text")  // 광고 문구
+    private String ad_text;
+
+    @Column(name = "image_url")  // 이미지 url
+    private String image_url;
+
 
 }
 
