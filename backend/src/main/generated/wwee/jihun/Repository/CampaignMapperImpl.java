4345fc8a50fa2f6363b1434ecaa92170e0cda0c4
eojiinn <djwls2726@gmail.com>
2024-11-05 17:02:12 +0900

    image 기능 코드

--- backend/src/main/generated/wwee/jihun/Repository/CampaignMapperImpl.java
+++ backend/src/main/generated/wwee/jihun/Repository/CampaignMapperImpl.java
@@ -1,4 +1,4 @@
-package wwee.jihun.Service;
+package wwee.jihun.Repository;
 
 import javax.annotation.processing.Generated;
 import org.springframework.stereotype.Component;
@@ -6,7 +6,7 @@ import wwee.jihun.Entity.CampaignEntity;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2024-10-25T09:27:09+0900",
+    date = "2024-11-05T16:52:11+0900",
     comments = "version: 1.5.3.Final, compiler: javac, environment: Java 17.0.12 (Ubuntu)"
 )
 @Component
@@ -18,17 +18,13 @@ public class CampaignMapperImpl implements CampaignMapper {
             return;
         }
 
-        target.setCampaignId( source.getCampaignId() );
-        target.setUserId( source.getUserId() );
         target.setProduct( source.getProduct() );
-        target.setKeyword1( source.getKeyword1() );
-        target.setKeyword2( source.getKeyword2() );
-        target.setKeyword3( source.getKeyword3() );
-        target.setBrand( source.getBrand() );
+        target.setKeywords( source.getKeywords() );
+        target.setFeatures( source.getFeatures() );
         target.setTone( source.getTone() );
-        target.setPrompt_for_ad_text( source.getPrompt_for_ad_text() );
-        target.setPrompt_for_ad_image( source.getPrompt_for_ad_image() );
+        target.setBrand( source.getBrand() );
+        target.setBrand_model( source.getBrand_model() );
         target.setAd_text( source.getAd_text() );
-        target.setAd_image( source.getAd_image() );
+        target.setImage_url( source.getImage_url() );
     }
 }
--- frontend/src/api/GptService.js
+++ frontend/src/api/GptService.js
@@ -34,16 +34,37 @@ export const GenerateAdText = async (token,product, brand, tone, brand_model, fe
     });
 }
 
-// //==================== 이미지 =======================
-// // image 생성 (> s3 버킷에 저장 > 이미지 url 반환)
-// export const GenerateAdImage = async (prompt, userId, campaignId) => {
-//     return await axios.post(`${API_URL}/image`, {
-//         prompt: prompt,
-//         userId: userId,
-//         campaignId: campaignId
-//     })
-// }
+//==================== 이미지 =======================
+// image 생성 (> s3 버킷에 저장 > 이미지 url 반환)
+export const GenerateImageUrl = async (token, prompt, userId, campaignId) => {
+    const response = await axios.post(`${API_URL}/image`, null, {
+        params: { 
+            prompt: prompt,
+            userId: userId,
+            campaignId: campaignId
+        },
+        headers: {
+            Authorization: `Bearer ${token}`,
+        }
+    });
+    return response.data;
+}
 
+
+export const onlyImage = async (token, prompt) => {
+    try {
+        const response = await axios.post(`${API_URL}/onlyImage`, null, {
+            params: { prompt: prompt },
+            headers: {
+                Authorization: `Bearer ${token}`,
+            },
+        });
+        return response.data;
+    } catch (error) {
+        console.error('이미지 받아오는 중 오류:', error);
+        throw error;
+    }
+};
 // // image 버킷에서 삭제
 // export const DeleteAdImage = async (prompt, userId, campaignId) => {
 //     return await axios.post(`${API_URL}/image-dlt`, {
