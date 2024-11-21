3ac05400fbdb2339416b46bf90bc25b01470f5fd
eojiinn <djwls2726@gmail.com>
2024-11-21 15:16:53 +0900

    최종ver1

--- frontend/src/api/UserService.js
+++ frontend/src/api/UserService.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-//const API_URL = `http://${process.env.VUE_APP_API_URL}/api/user`;
-const API_URL = `http://localhost:8080/api/user`;
+const API_URL = `http://${process.env.VUE_APP_API_URL}/api/user`;
+// const API_URL = `http://localhost:8080/api/user`;
 
 // 회원가입 API 호출
 export const registerUser = async (payload) => {
